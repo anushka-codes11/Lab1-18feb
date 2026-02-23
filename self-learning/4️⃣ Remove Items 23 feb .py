b62e00bf0fe5daf3a14187643cc5833e8d31c8db
anushka-codes11 <anshipandit631@gmail.com>
2026-02-23 21:59:50 +0530

    Update 4️⃣ Remove Items 23 feb .py

--- self-learning/4️⃣ Remove Items 23 feb .py
+++ self-learning/4️⃣ Remove Items 23 feb .py
@@ -1,5 +1,9 @@
-# Removing items
+ # Removing items
 student = {"name": "John", "age": 20, "grade": "A"}
 student.pop("grade")       # Remove specific key
 del student["age"]         # Delete key
-print("After removal:", student)
+
+print("After removal:", student) 
+
+// Output : 
+After removal: {'name': 'John'}
