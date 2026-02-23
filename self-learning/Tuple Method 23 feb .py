ac6a0fb0958030885c1f1939b5b8a29e7b27d198
anushka-codes11 <anshipandit631@gmail.com>
2026-02-23 21:26:24 +0530

    Update Tuple Method 23 feb .py

--- self-learning/Tuple Method 23 feb .py
+++ self-learning/Tuple Method 23 feb .py
@@ -1,4 +1,9 @@
 # Using count() and index()
 numbers = (1, 2, 3, 2, 4, 2)
 print("Count of 2:", numbers.count(2))
-print("Index of 3:", numbers.index(3))
+
+print("Index of 3:", numbers.index(3)) 
+
+// output : 
+Count of 2:3
+Index of 3:2
